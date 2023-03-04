f3e89925b5d5b938d3772c083d2709c1725515e7
Matheus <math.av7@outlook.com>
2023-03-03 22:40:32 -0300

    feat<useCase>: finishing useCase of user creation

--- src/useCases/createUser/create-user.useCase.ts
+++ src/useCases/createUser/create-user.useCase.ts
@@ -1,11 +1,18 @@
 import { User } from "../../entities/user";
+import { IMailProvider } from "../../providers/mail-provider.interface";
 import { IUsersRepository } from "../../repositories/users.repository.interface";
 import { ICreateUserRequesDTO } from "./create-user.dto";
+
+// L: Liskov Substitution Principle
+// D: Dependency Inversion Principle
 
 export class CreateUserUseCase {
   // using an interface in the repository, you can already code your useCases (service)
   // without implementing the interface
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailtrapProvide: IMailProvider
+  ) {}
 
   public async execute(data: ICreateUserRequesDTO) {
     const userAlreadyExists = await this.usersRepository.findByEmail(
@@ -19,5 +26,18 @@ export class CreateUserUseCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    await this.mailtrapProvide.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: "Equipe do Meu App",
+        email: "equipe@meuapp.com",
+      },
+      subject: "Seja bem-vindo(a)",
+      body: "<p>Agora voce pode logar em Meu App</p>",
+    });
   }
 }
