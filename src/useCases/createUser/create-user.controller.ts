59ca69eb40d01fd4fc33c14a1ecc4a5adfc2badf
Matheus <math.av7@outlook.com>
2023-03-03 22:41:35 -0300

    feat<controller>: establishing client controller for user creation

---
+++ src/useCases/createUser/create-user.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { CreateUserUseCase } from "./create-user.useCase";
+
+export class CreateUserController {
+  constructor(private CreateUserUseCase: CreateUserUseCase) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    try {
+      await this.CreateUserUseCase.execute({
+        name,
+        email,
+        password,
+      });
+
+      return response.status(201).send();
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message || "Unexpected error.",
+      });
+    }
+  }
+}
